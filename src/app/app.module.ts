956156e4a66d66bf7f5786dce1999fe2958065bd
Lazz <alaa_est1919@hotmail.fr>
2021-07-14 14:36:36 +0200

    final version for data collection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { File } from '@ionic-native/file/ngx';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  Geolocation,File
+  Geolocation,File,BluetoothSerial
   ],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
-import { Component, NgZone } from '@angular/core';
+import { Component} from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { File } from '@ionic-native/file/ngx';
+import { ToastController, LoadingController, AlertController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,25 +14,84 @@ export class HomePage {
   longitude: any = 0; //longitude
   table= [] ;
   dir= 'file:///storage/emulated/0/';
-  filename = 'GPSDATA';
+  filename = '';
   object;
+  devices: any[] = [];
+  gsr;
+  showToggle:boolean = true;
+  coToggle:boolean = true;
+  interval;
+  color = "red";
   constructor(
     private geolocation: Geolocation,
-    public file: File
-  ) {}
+    public file: File,
+    private toastCtrl: ToastController,
+    public blserial: BluetoothSerial,
+    public loadingController: LoadingController,
+    public alertController: AlertController
+  ) {
+    
+  }
   options = {
     timeout: 10000, 
     enableHighAccuracy: true, 
     maximumAge: 3600
   };
 
+async  toastConnect(resp){
+    let toast = await this.toastCtrl.create({
+      message: resp,
+      duration: 3000
+    });                     
+  await  toast.present()
+  }
+
+
+  connect() {
+    this.toastConnect('Connecting to Micro-controller...')
+    this.blserial.connect("08:3A:F2:AC:B6:82").subscribe( (connectSuccess) => {                  
+      console.log(connectSuccess);
+      this.toastConnect('Succesfully connected')
+      
+
+    }, connectCallback => {
+      console.log('disconnected');
+      this.toastConnect('Connection failed')
+    })
+
+  }
+
+  disconnect(){
+    this.toastConnect('Disconnecting from Micro-controller...')
+    this.blserial.disconnect().then((success) => {
+      console.log(success);
+      this.toastConnect('Disconnected')
+      this.coToggle = true;
+
+    }, error =>{
+      console.log('error disconnecting')
+      this.toastConnect('Error while disconnecting')
+    })
+  }
+
+
+startRecording(){
+  if (this.filename == '') {
+    this.presentAlertPrompt()
+  }
+  else this.getCurrentCoordinates();
+}
+
 // use geolocation to get user's device coordinates
 getCurrentCoordinates() {
-  setInterval(() => {
+this.interval=  setInterval(() => {
+    this.blserial.read().then((success)=>{
+      this.gsr= success;
+    })
   this.geolocation.getCurrentPosition({ enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 }).then((resp) => {
     this.latitude = resp.coords.latitude;
     this.longitude = resp.coords.longitude;
-   var coordonate= {"position_timestamp": new Date().toISOString(), 'latitude' : this.latitude,'longitude': this.longitude};
+   var coordonate= {"position_timestamp": new Date().toISOString(), 'latitude' : this.latitude,'longitude': this.longitude,'GSR' : this.gsr };
     console.log(coordonate)
     this.table.push(coordonate)
     console.log(this.table)
@@ -37,9 +99,88 @@ getCurrentCoordinates() {
      console.log('Error getting location', error);
    })
   }, 3000);
+  this.interval;
 }
-saveData(){
+
+
+stopRecord(){
+  clearInterval(this.interval);
+}
+
+resetData(){
+  clearInterval(this.interval);
+  this.table.splice(0,this.table.length);
+  this.latitude = 0;
+  this.longitude = 0;
+  this.gsr = 0;
+  this.filename = ''
+  this.showToggle = true;
+}
+
+async saveData(){
+  let loading = await this.loadingController.create({
+    cssClass: 'my-custom-class',
+    message: 'Saving ...',
+    duration: 1500
+  });
+  await loading.present();
   this.object = Object.assign({}, this.table);
+  console.log(this.filename)
   this.file.writeFile(this.dir,this.filename,this.object,{replace: true})
+}
+showToggles(){
+  if(this.showToggle == true){
+    this.showToggle = false;
+  }else{
+    this.showToggle = true;
+  }
+}
+coToggles(){
+  if(this.coToggle == true){
+    this.coToggle = false;
+  }else{
+    this.coToggle = true;
+  }
+}
+async presentAlertPrompt() {
+  const alert = await this.alertController.create({
+    header: 'Type subject name',
+    inputs: [
+      {
+        type: 'text',
+        placeholder: 'file name'
+      }
+    ],
+    buttons: [
+      {
+        text: 'Ok',
+        handler: (name) => {
+          this.filename=name[0];
+          this.getCurrentCoordinates()
+          console.log(this.filename)
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
+async resetAlert() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Non saved data will be lost',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+      }, {
+        text: 'Reset',
+        handler: () => {
+         this.resetData();
+        }
+      }
+    ]
+  });
+
+  await alert.present();
 }
 }
